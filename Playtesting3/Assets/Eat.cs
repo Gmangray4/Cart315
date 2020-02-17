4e7736d21a241314363bfbb243299e64c9aa9358
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2020-02-16 23:29:29 -0500

    NewPlaytest3thatworks!!!!

---
+++ Playtesting3/Assets/Eat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour
+{
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("OnCollisionEnter works.");
+        if (col.gameObject.name == "FPSController")
+        {
+            Debug.Log("The GameObject name is Cube.");
+            Destroy(col.gameObject);
+        }
+    }
+
+}
