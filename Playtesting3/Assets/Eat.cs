203da2eaea94fb6a1884355cf6643d2cee227598
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2020-02-18 20:32:01 -0500

    PlayTest33333333 done\\\\\\\\\\\!

--- Playtesting3/Assets/Eat.cs
+++ Playtesting3/Assets/Eat.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Eat : MonoBehaviour
 {
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform respawnPoint;
 
-
-    void OnTriggerEnter(Collider col)
+    void OnTriggerEnter(Collider Player)
     {
         Debug.Log("OnCollisionEnter works.");
-        if (col.gameObject.name == "FPSController")
+        if (Player.gameObject.name == "FPSController")
         {
             Debug.Log("The GameObject name is Cube.");
-            Destroy(col.gameObject);
+            Player.transform.position = respawnPoint.transform.position;
+         
         }
     }
 
